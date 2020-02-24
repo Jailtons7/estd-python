4930e6755083897f7272e91b9b492b0b149228f4
Jailton Silva <jailtonsouza7@gmail.com>
2020-02-23 22:31:16 -0300

    Initial commit

---
+++ lista_funcoes.py
@@ -0,0 +1,59 @@
+def fatorial(n):
+    '''
+    :param n: inteiro
+    :return: inteiro, fatorial de n
+    '''
+    if n < 0:
+        print('não existe fatorial de um número negativo')
+    elif n < 2:
+        return 1
+    else:
+        return n * fatorial(n-1)
+
+def eh_par(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+def par_ou_impar(n):
+    if n % 2 == 0:
+        return 'par'
+    else:
+        return 'ímpar'
+
+def compare(a, b):
+    if a > b:
+        return 1
+    elif a == b:
+        return 0
+    else:
+        return -1
+
+def maior_de_2(num_1, num_2):
+    if num_1 > num_2:
+        return num_1
+    else:
+        return num_2
+
+def maior_de_3(a,b,c):
+    if a >= b:
+        if a > c:
+            return a
+        else:
+            return c
+    else:
+        if b > c:
+            return b
+        else:
+            return c
+def dia_da_semana(num):
+    numeros = [1,2,3,4,5,6,7]
+    dias = ['domingo', 'segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sábado']
+    if num in numeros:
+        indice = numeros.index(num)
+        return dias[indice]
+    else:
+        return 'Dia inválido'
+
+
---
+++ lista_strings.py
@@ -0,0 +1,123 @@
+# Neste módulo estão as funções sobre a lista 2 - strings.
+# O desafio é usar o mínimo possível de funções built-in do Python
+
+def comprimento(iteravel):
+    '''
+    :param iteravel: pode ser string, lista, tupla...
+    :return: tamanho da string (minha versão da função len())
+    '''
+    contador = 0
+    for i in iteravel:
+        contador += 1
+    return contador
+
+def eh_palindroma(palavra):
+    '''
+    :param palavra: string
+    :return: True, se a palavra for palíndroma e False, caso contrário.
+    '''
+    tamanho = comprimento(palavra)
+    for i in range(0, tamanho-1):
+       if (palavra[i] != palavra[-i-1]):
+           return False
+    return True
+
+def retira_espaco(frase):
+    '''
+    :param frase: string
+    :return: Frase sem espaços no início e no fim
+    '''
+    tamanho = comprimento(frase)
+    cont_ini = 0
+    cont_fim = 0
+    for i in range(0, tamanho-1):
+        if frase[i] == ' ':
+            cont_ini += 1
+        else:
+            break
+    for i in range(1, tamanho-1):
+        if frase[-i] == ' ':
+            cont_fim += 1
+        else:
+            break
+    final = tamanho - cont_fim
+    return frase[cont_ini:final]
+
+def quantidade_palavras(texto):
+    '''
+    :param texto: string
+    :return: quantidade de palavras no texto
+    '''
+    texto_liquido = retira_espaco(texto)
+    tamanho = comprimento(texto_liquido)
+    contador = 1
+    anterior = ''
+    for i in range(0, tamanho):
+        if texto_liquido[i] == ' ' and anterior != ' ' and anterior != '\n':
+            contador += 1
+        anterior = texto_liquido[i]
+    return contador
+
+def ultimo_nome(nome_completo):
+    '''
+    :param nome_completo: string
+    :return: string, ultimo nome
+    '''
+    nome = retira_espaco(nome_completo)
+    tamanho = comprimento(nome)
+    contador = 0
+    for i in range(1, tamanho+1):
+        if nome[-i] != ' ':
+            contador += 1
+        else:
+            break
+    fatiamento = tamanho - contador
+    return nome[fatiamento:]
+
+def formato_aereas(nome_completo):
+    '''
+    :param nome_completo: String
+    :return: String, nome no formato das companhias aéreas
+    '''
+    nome = retira_espaco(nome_completo)
+    primeiro = ''
+    for letra in nome:
+        if letra != ' ':
+            primeiro += letra
+        else:
+            break
+    ultimo = ultimo_nome(nome)
+    formato = ultimo + '/' + primeiro
+    return formato
+print(formato_aereas('Jailton de souza silva'))
+def para_maiuscula(frase):
+    '''
+    :param frase: String
+    :return: String, a frase em maiúsculas
+    '''
+    minusculas = ['a','à','á','ã','â','b','c','ç','d','e','é','ê','f','g','h','i','í','j','k','l','m',
+                  'n','o','ó','ô','õ','p','q','r','s','t','u','ú','v','w','x','y','z']
+    maiusculas = ['A','À','Á','Ã','Â','B','C','Ç','D','E','É','Ê','F','G','H','I','Í','J','K','L','M',
+                  'N','O','Ó','Ô','Õ','P','Q','R','S','T','U','Ú','V','W','X','Y','Z']
+
+    resultado = ''
+    for letra in frase:
+        if letra in minusculas:
+            indice = minusculas.index(letra)
+            resultado += maiusculas[indice]
+        else:
+            resultado += letra
+    return resultado
+
+def formato_referencia(nome_completo):
+    '''
+    :param nome_completo: String
+    :return: String, nome no formato de referência bibliográfica
+    '''
+    ultimo = para_maiuscula(ultimo_nome(nome_completo))
+    alcance = comprimento(nome_completo) - comprimento(ultimo)
+    nome_aux = para_maiuscula(nome_completo[0])+'.'
+    for i in range(alcance-1):
+        if nome_completo[i] == ' ':
+            nome_aux += para_maiuscula(nome_completo[i+1])+'.'
+    return '%s,%s' % (ultimo, nome_aux)
