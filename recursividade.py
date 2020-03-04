7a71380d39989fce8ce5183cfb88eb7761ed9a1b
Jailton Silva <jailtonsouza7@gmail.com>
2020-03-03 23:29:29 -0300

    update recursividade.py and lista_funcoes.py

--- recursividade.py
+++ recursividade.py
@@ -1,4 +1,5 @@
 # Soluções da lista sobre recursividade
+
 from lista_strings import comprimento
 
 def fatorial(n):
@@ -9,15 +10,42 @@ def fatorial(n):
 
 
 def inverte_palavra(palavra):
+    '''
+    :param palavra: string
+    :return: string, a palavra invertida
+    '''
     tamanho = comprimento(palavra)
-    invertida = palavra[tamanho-1]
-    nova_palavra = palavra[0:tamanho-2]
-    return invertida + inverte_palavra[nova_palavra]
-
-
-def inverte_palavra(palavra):
-    tamanho = len(palavra)
     if tamanho >= 1:
-      invertida = palavra[tamanho-1]
-      nova_palavra = palavra[0:tamanho-1]
-      return invertida + str(inverte_palavra(nova_palavra))
+        invertida = palavra[-1]
+        nova_palavra = palavra[0:tamanho-1]
+        return invertida + str(inverte_palavra(nova_palavra))
+    else:
+        return ''
+
+
+def conta_letra(letra, palavra):
+    '''
+    :param letra, palavra: string
+    :return: inteiro, quantas vezes a "letra" aparece na "palavra"
+    '''
+    tamanho = comprimento(palavra)
+    if tamanho >= 1:
+        if palavra[0] == letra:
+            nova_palavra = palavra[1:]
+            return 1 + conta_letra(letra, nova_palavra)
+        else:
+            nova_palavra = palavra[1:]
+            return conta_letra(letra, nova_palavra)
+    else:
+        return 0
+
+
+def n_primeiros_decrescente(numero):
+    '''
+    :param numero: inteiro
+    :return: None, apenas imprime os n primeiros números em ordem crescente começando do zero
+    '''
+    if numero >= 0:
+        novo_numero = numero - 1
+        print(novo_numero+1)
+        n_primeiros_decrescente(novo_numero)
